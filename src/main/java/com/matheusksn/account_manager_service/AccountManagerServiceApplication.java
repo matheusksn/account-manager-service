660df998637505a5e05c17cae0af813b0d1b85ca
Matheus <101477339+matheusksn@users.noreply.github.com>
2025-05-15 21:55:01 -0300

    primeiro commit

---
+++ src/main/java/com/matheusksn/account_manager_service/AccountManagerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.matheusksn.account_manager_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountManagerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountManagerServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/matheusksn/account_manager_service/AccountManagerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheusksn.account_manager_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountManagerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
